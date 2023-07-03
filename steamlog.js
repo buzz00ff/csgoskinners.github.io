a610e6c0605d93aec4a11183b25186a2dc58b356
buzz00ff <137896111+buzz00ff@users.noreply.github.com>
2023-07-03 12:43:36 +0200

    Delete steamlog.js

--- steamlog.js
+++
@@ -1,8 +0,0 @@
-// Funkcja do obsługi kliknięcia przycisku logowania
-function handleLogin() {
-  // Przekierowanie użytkownika do strony logowania Steam
-  window.location.href = "https://steamcommunity.com/openid/login";
-}
-
-// Nasłuchiwanie zdarzenia kliknięcia przycisku logowania
-document.getElementById("login-button").addEventListener("click", handleLogin);
